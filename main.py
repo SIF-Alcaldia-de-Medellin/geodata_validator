cdcb63b635803c6f96353afc13e1d71e9be60528
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-09-10 10:27:47 -0500

    Feature: Input name of shapefile saved in the shape folder

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
 from utils.functions import *
 from utils.validators import *
+import warnings
+
+warnings.filterwarnings("ignore")
+
+shapefile = input("Ingrese el nombre de la capa a usar sin extensión(.shp): ")
 
 current_dir = os.getcwd()
-shapefile_path = os.path.join(current_dir, "shapes", "shape_puentes.shp")
+shapefile_path = os.path.join(current_dir, "shapes", f"{shapefile}.shp")
 
 gdf = load_geodataframe(shapefile_path)
 
@@ -27,9 +32,9 @@ try:
     gdf = change_field_names(gdf)
     gdf = transform_nulls(gdf)
 
-    gdf.to_file(os.path.join(current_dir,"shapes", "new_file.shp"))
+    gdf.to_file(os.path.join(current_dir,"shapes", f"{shapefile}_fixed"))
 
-    print("Se ha guardado el shapefile con los nombres de columna en minúsculas y corregido los valores nulos.")
+    print(f"Se ha guardado el shapefile con los nombres de columna en minúsculas y los valores nulos corregidos. La encontraras en la carpeta: {shapefile}_fixed")
 except Exception as e:
     print(f"Ha ocurrido un error al guardar el shapefile: {e}")
     exit(1)
